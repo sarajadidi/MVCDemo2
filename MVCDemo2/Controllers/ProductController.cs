fe278269b771c25f0dced3da1dd7d8c5f1de4e3f
sarajadidi <sararivkajadidi@gmail.com>
2023-11-08 11:07:52 -0800

    created and finished demo x2

---
+++ MVCDemo2/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCDemo2.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var products = _repo.GetAllProducts();
+            return View(products);
+        }
+    }
+}
+
---
+++ MVCDemo2/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MVCDemo2.Models;
+
+namespace MVCDemo2
+{
+	public interface IProductRepository
+	{
+        public IEnumerable<Product> GetAllProducts();
+
+    }
+}
+
